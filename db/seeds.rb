2df722eb46f5ae7b039fd45a98f1375e1b65afe3
Garrett <garres4@vt.edu>
2021-12-12 11:46:29 -0700

    commit tree 5ea2256d3bc960b7b37088620125b673eef5393d

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create(first_name: "Jerry", last_name: "Smith", grade: 11)
+Student.create(first_name: "John", last_name: "Jacobs", grade: 12)
+Student.create(first_name: "Tom", last_name: "Barry", grade: 9)
